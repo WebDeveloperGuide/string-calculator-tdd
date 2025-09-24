6cd063d4baa2a24336e993a85d24b6533e5e37f9
WebDeveloperGuide <support@webdeveloperguide.in>
2025-09-24 21:33:21 +0530

    - single number test case added

--- __tests__/stringCalculator.test.ts
+++ __tests__/stringCalculator.test.ts
@@ -4,4 +4,8 @@ describe('String Calculator - add()', () => {
   it('returns 0 for empty string', () => {
     expect(add('')).toBe(0);
   });
+
+  it('returns the number for a single numeric string', () => {
+    expect(add('1')).toBe(1);
+  });
 });
